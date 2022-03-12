71de2c2f34d0f943cab9810de8edf088ecb1fc7e
Wei Wei <weiwei@geekteahouse.cn>
2022-03-12 18:53:21 +0800

    Remove the property & propertyOf test cases

--- src/__tests__/Functor.test.ts
+++ src/__tests__/Functor.test.ts
@@ -1,4 +1,4 @@
-import { fmap, property, propertyOf } from "../Functor";
+import { fmap } from "../Functor";
 import { mutable, startWith } from "../Observable";
 import { inc } from "./TestHelper";
 
@@ -27,38 +27,6 @@ describe("Functor", () => {
       ob1.unobserve();
       ob1.unobserve();
       ob2.unobserve();
-      expect(unobserve).toBeCalledTimes(1);
-    });
-  });
-
-  describe("#property & #propertyOf", () => {
-    it("should extract a property of observable object", () => {
-      const unobserve = jest.fn();
-      const obj = mutable(startWith({ x: 1, y: 5 }, unobserve));
-      const x = property<{ x: number }>("x")(obj);
-      const y = propertyOf(obj)("y");
-
-      const cbX = jest.fn();
-      const obX = x.observe(cbX);
-
-      const cbY = jest.fn();
-      const obY = y.observe(cbY);
-
-      expect(obX.state).toBe(1);
-
-      obj.update(({ x, y }) => ({ x, y: y - 1 }));
-      expect(cbX).not.toBeCalled();
-      expect(cbY).toBeCalledTimes(1);
-      expect(cbY).toBeCalledWith(4);
-
-      obj.update(({ x, y }) => ({ x: x + 1, y }));
-      expect(cbY).toBeCalledTimes(1);
-      expect(cbX).toBeCalledTimes(1);
-      expect(cbX).toBeCalledWith(2);
-
-      // Cascaded unobserve
-      obX.unobserve();
-      obY.unobserve();
       expect(unobserve).toBeCalledTimes(1);
     });
   });
