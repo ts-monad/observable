c73f32c10979eb8f2056d62e9a8602195a73f0bf
Wei Wei <weiwei@geekteahouse.cn>
2022-03-15 11:46:26 +0800

    Add test for relay

--- src/__tests__/Store.test.ts
+++ src/__tests__/Store.test.ts
@@ -1,39 +1,71 @@
-import { mutable } from "../Store";
+import { mutable, relay } from "../Store";
 import { inc } from "./TestHelper";
 
 describe("Store", () => {
-  it("should create a store object", () => {
-    const sto = mutable(0);
+  describe("mutable", () => {
+    it("should create a mutable store", () => {
+      const mut = mutable(0);
 
-    // Update without observers
-    expect(inc(sto)).toBe(1);
-    expect(sto.isObserved()).toBe(false);
+      // Update without observers
+      expect(inc(mut)).toBe(1);
+      expect(mut.isObserved()).toBe(false);
 
-    // First observe
-    const cb1 = jest.fn();
-    const obn1 = sto.observe(cb1);
-    expect(obn1.value).toBe(1);
-    expect(sto.get()).toBe(1);
-    expect(sto.isObserved()).toBe(true);
+      // First observe
+      const cb1 = jest.fn();
+      const obn1 = mut.observe(cb1);
+      expect(obn1.value).toBe(1);
+      expect(mut.get()).toBe(1);
+      expect(mut.isObserved()).toBe(true);
 
-    // Notify the change
-    expect(inc(sto)).toBe(2);
-    expect(sto.get()).toBe(2);
-    expect(cb1).toBeCalledTimes(1);
-    expect(cb1).toHaveBeenLastCalledWith(2);
+      // Notify the change
+      expect(inc(mut)).toBe(2);
+      expect(mut.get()).toBe(2);
+      expect(cb1).toBeCalledTimes(1);
+      expect(cb1).toHaveBeenLastCalledWith(2);
 
-    // Cascaded unobserve
-    obn1.unobserve();
-    expect(sto.isObserved()).toBe(false);
+      // Cascaded unobserve
+      obn1.unobserve();
+      expect(mut.isObserved()).toBe(false);
 
-    // Second observation
-    const cb2 = jest.fn();
-    const obn2 = sto.observe(cb2);
-    expect(obn2.value).toBe(2);
-    expect(sto.isObserved()).toBe(true);
+      // Second observation
+      const cb2 = jest.fn();
+      const obn2 = mut.observe(cb2);
+      expect(obn2.value).toBe(2);
+      expect(mut.isObserved()).toBe(true);
 
-    // Cascaded unobserve
-    obn2.unobserve();
-    expect(sto.isObserved()).toBe(false);
+      // Cascaded unobserve
+      obn2.unobserve();
+      expect(mut.isObserved()).toBe(false);
+    });
+  });
+
+  describe("relay", () => {
+    it("should create a relay store", () => {
+      const mut = mutable(0);
+      const rly = relay(mut);
+
+      expect(mut.isObserved()).toBeTruthy();
+      expect(rly.isObserved()).toBeFalsy();
+
+      // Should relay the value
+      const cb = jest.fn();
+      const obn = rly.observe(cb);
+      expect(obn.value).toBe(0);
+      expect(rly.isObserved()).toBeTruthy();
+
+      // Should relay the updates
+      inc(mut);
+      expect(cb).toHaveBeenLastCalledWith(1);
+      inc(mut);
+      expect(cb).toHaveBeenLastCalledWith(2);
+
+      // Unobserve the relay
+      obn.unobserve();
+      expect(mut.isObserved()).toBeTruthy();
+      expect(rly.isObserved()).toBeFalsy();
+
+      rly.destroy();
+      expect(mut.isObserved()).toBeFalsy();
+    });
   });
 });
